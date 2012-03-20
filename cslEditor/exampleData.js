ba763804d013e04e3a0be03e75248eb090065918
Steve Ridout <steveridout@gmail.com>
2012-03-20 20:15:35 +0100

    forgot from last commit. refs #70

---
+++ cslEditor/exampleData.js
@@ -0,0 +1,191 @@
+var cslEditorExampleData = {
+	jsonDocuments: {
+		"ITEM-1": {
+			"author": [{
+				"family": "Brown",
+				"given": "Emmett"
+			}],
+			"container-title": "The Journal of Applied Relativistic Physics",
+			"editor": [],
+			"id": "ITEM-1",
+			"issue": "2",
+			"issued": {
+				"date-parts": [
+					["1985"]
+				]
+			},
+			"page": "88-102",
+			"title": "The Temporal Effects of the Flux Capacitor",
+			"translator": [],
+			"type": "article-journal",
+			"volume": "45"
+		},
+		"ITEM-2": {
+			"DOI": "10.1038/119558a0",
+			"URL": "http://www.nature.com/doifinder/10.1038/119558a0",
+			"accessed": {
+				"date-parts": [
+					["2011", "6", "7"]
+				]
+			},
+			"author": [{
+				"family": "Davisson",
+				"given": "C."
+			}, {
+				"family": "Germer",
+				"given": "L. H."
+			}],
+			"container-title": "Nature",
+			"editor": [],
+			"id": "ITEM-2",
+			"issue": "2998",
+			"issued": {
+				"date-parts": [
+					["1927", "4", "16"]
+				]
+			},
+			"page": "558-560",
+			"title": "The Scattering of Electrons by a Single Crystal of Nickel",
+			"translator": [],
+			"type": "article-journal",
+			"volume": "119"
+		},
+		"ITEM-3": {
+			"DOI": "10.1088/0143-0807/27/4/007",
+			"URL": "http://bavard.fourmilab.ch/etexts/einstein/specrel/specrel.pdf",
+			"abstract": "General description of special relativity",
+			"author": [{
+				"family": "Einstein",
+				"given": "Albert"
+			}],
+			"chapter-number": "3",
+			"container-title": "Annalen der Physik",
+			"editor": [],
+			"id": "ITEM-3",
+			"issue": "4",
+			"issued": {
+				"date-parts": [
+					["1905"]
+				]
+			},
+			"page": "1-26",
+			"publisher": "Dover Publications",
+			"title": "On the electrodynamics of moving bodies",
+			"translator": [],
+			"type": "article-journal",
+			"volume": "17"
+		},
+		"ITEM-4": {
+			"DOI": "10.1038/171737a0",
+			"URL": "http://www.ncbi.nlm.nih.gov/pubmed/13054692",
+			"abstract": "We wish to suggest a structure for the salt of deoxyribose nucleic acid (D.N.A.). This structure has novel features which are of considerable biological interest.",
+			"author": [{
+				"family": "Watson",
+				"given": "J D"
+			}, {
+				"family": "Crick",
+				"given": "F H"
+			}],
+			"container-title": "Nature",
+			"editor": [],
+			"id": "ITEM-4",
+			"issue": "4356",
+			"issued": {
+				"date-parts": [
+					["1953"]
+				]
+			},
+			"page": "737-738",
+			"publisher": "Am Med Assoc",
+			"title": "Molecular structure of nucleic acids; a structure for deoxyribose nucleic acid.",
+			"translator": [],
+			"type": "article-journal",
+			"volume": "171"
+		},
+		"ITEM-5": {
+			"author": [{
+				"family": "Acemoglu",
+				"given": "D S"
+			}, {
+				"family": "Johnson",
+				"given": "S"
+			}, {
+				"family": "Robinson",
+				"given": "J A"
+			}],
+			"container-title": "NBER Working Papers 8460",
+			"editor": [],
+			"id": "ITEM-5",
+			"issued": {
+				"date-parts": [
+					["2001"]
+				]
+			},
+			"publisher": "National Bureau of Economic Research",
+			"publisher-place": "Washington D.C.",
+			"title": "Reversal of Fortune: Geography and Institutions in the Making of the Modern World Income Distribution",
+			"translator": [],
+			"type": "article-journal"
+		},
+		"ITEM-6": {
+			"author": [{
+				"family": "Acemoglu",
+				"given": "D S"
+			}, {
+				"family": "Johnson",
+				"given": "S"
+			}, {
+				"family": "Robinson",
+				"given": "J A"
+			}],
+			"container-title": "The Quarterly Journal of Economics",
+			"editor": [],
+			"id": "ITEM-6",
+			"issue": "4",
+			"issued": {
+				"date-parts": [
+					["2002"]
+				]
+			},
+			"page": "1231-1294",
+			"title": "Reversal of Fortune: Geography and Institutions in the Making of the Modern World Income Distribution",
+			"translator": [],
+			"type": "article-journal",
+			"volume": "117"
+		}
+	},
+	citationsItems: [{
+		"citationId": "CITATION-0",
+		"citationItems": [{
+			"id": "ITEM-1",
+			"uris": [ /*"http://www.mendeley.com/documents/?uuid=dd1a39c6-e14a-4c34-8edb-98ef1731e557" */ ]
+		}],
+		"mendeley": {
+			"previouslyFormattedCitation": "(Brown, 1985)"
+		},
+		"properties": {
+			"noteIndex": 0
+		},
+		"schema": "https://github.com/citation-style-language/schema/raw/master/csl-citation.json"
+	}, {
+		"citationId": "CITATION-1",
+		"citationItems": [{
+			"id": "ITEM-2",
+			"uris": ["http://www.mendeley.com/documents/?uuid=a030fc65-aabc-4b12-a454-a917280affc1"]
+		}, {
+			"id": "ITEM-3",
+			"uris": ["http://www.mendeley.com/documents/?uuid=4d771e0b-52a1-4561-9e39-260877d1db08"]
+		}, {
+			"id": "ITEM-4",
+			"uris": ["http://www.mendeley.com/documents/?uuid=6bdb385b-a342-454e-a838-4dde5f697f0a"]
+		}],
+		"mendeley": {
+			"previouslyFormattedCitation": "(Davisson &#38; Germer, 1927; Einstein, 1905; Watson &#38; Crick, 1953)"
+		},
+		"properties": {
+			"noteIndex": 0
+		},
+		"schema": "https://github.com/citation-style-language/schema/raw/master/csl-citation.json"
+	}]
+};
+
