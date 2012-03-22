4e1fd82babf91eb1c788e775ca408ad62b15af8c
Steve Ridout <steveridout@gmail.com>
2012-03-22 18:21:47 +0100

    test using jstree to display CSL structure as tree. refs #71

--- cslEditor/index.php
+++ cslEditor/index.php
@@ -6,6 +6,8 @@
 	<title>CSL IDE</title>
 
 	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script src="http://code.jquery.com/ui/1.8.18/jquery-ui.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="http://code.jquery.com/ui/1.8.18/themes/ui-lightness/jquery-ui.css">
 
 	<link rel="stylesheet" href="./codemirror.css">
 	<script src="../external/codemirror2/lib/codemirror.js"></script>
@@ -20,11 +22,20 @@
 	<script type="text/javascript" src="../external/citeproc/runcites.js"></script>
 	<script type="text/javascript" src="../external/diff-match-patch/diff_match_patch.js"></script>
 
+	<!--script type="text/javascript" src="../external/jstree/_lib/jquery.hotkeys.js"></script-->
+	<script type="text/javascript" src="../external/jstree/jquery.jstree.js"></script>
+	<link type="text/css" rel="stylesheet" href="../external/jstree/themes/default/style.css"/>
+
 	<script type="text/javascript" src="../src/citationEngine.js"></script>
 	<script type="text/javascript" src="exampleData.js"></script>
 	<script type="text/javascript" src="../src/diff.js"></script>
+	<script type="text/javascript" src="../src/cslJSON.js"></script>
 
 	<style type="text/css">
+		html, body {
+			width: 98%;
+			height: 98%;
+		}
 		#code {
 			border: 1px solid #eee;
 			overflow: auto;
@@ -32,28 +43,59 @@
 		.searched {
 			background: yellow;
 		}
+		#treeEditor {
+			font-size: 14px;
+			height: 86%;
+			width: 90%;
+			overflow: auto;
+		}
+		#editorTabs {
+			float: left;
+			width: 35%;
+			height: 90%;
+			/*overflow: auto;*/
+		}
+		#output {
+			float: right;
+			width: 63%;
+			height: 95%;
+			overflow: auto;
+		}
 	</style>
 </head>
 <body>
-<h1>CSL IDE</h1>
+<!--<h1>CSL IDE</h1>-->
 <!--<input type="file" id="files" name="files[]" />-->
 <output id="list"></output>
 
-<form name="codeForm">
-	<textarea id="code" name="code">
-	</textarea>
-</form>
+<div id="editorTabs">
+	<ul>
+		<li><a href="#treeEditor">Tree editor</a></li>
+		<li><a href="#codeEditor">Code view</a></li>
+	</ul>
+	<div id="treeEditor">
+		<!--div id="innerTreeEdit">
+		</div-->
+	</div>
+	<div id="codeEditor"-->
+		<form name="codeForm">
+			<textarea id="code" name="code">
+			</textarea>
+		</form>
+	</div>
+</div>
 
-<div id="statusMessage"></div>
+<div id="output">
+	<div id="statusMessage"></div>
 
-<h3>Formatted Citations</h3>	
-<div id="formattedCitations"></div>
+	<h3>Formatted Citations</h3>	
+	<div id="formattedCitations"></div>
 
-<h3>Formatted Bibliography</h3>
-<div id="formattedBibliography"></div>
+	<h3>Formatted Bibliography</h3>
+	<div id="formattedBibliography"></div>
+</div>
 
 <script>
-
 "use strict";
 
 var CSLEDIT = CSLEDIT || {};
@@ -116,8 +158,111 @@ CSLEDIT.editorPage = (function () {
 		document.getElementById("statusMessage").innerHTML = formattedResult.statusMessage;
 	};
 
+	var assertEqual = function (actual, expected, place) {
+		if (actual !== expected) {
+			alert("assert fail at " + place + "\n" +
+				actual + " !== " + expected);
+		}
+	};
+
+	var updateTreeView = function (xmlData) {
+		var parser = new DOMParser();
+		var xmlDoc = parser.parseFromString(xmlData, "application/xml");
+
+		var styleNode = xmlDoc.childNodes[0];
+		assertEqual(styleNode.localName, "style");
+
+		var jsonData = parseNode(styleNode);
+
+		$("#treeEditor").jstree({
+			"json_data" : { data : [ jsonData ] },
+			"types" : {
+				"valid_children" : [ "root" ],
+				"types" : {
+					"text" : {
+						"icon" : {
+							"image" : "http://static.jstree.com/v.1.0rc/_docs/_drive.png"
+						}
+					}
+				}
+			},
+				
+			"plugins" : ["themes","json_data","ui", "crrm", "dnd", "contextmenu", "types"],
+			// each plugin you have included can have its own config object
+			"core" : { "initially_open" : [ "node1" ] }
+			// it makes sense to configure a plugin only if overriding the defaults
+		});
+
+	};
+
+	var parseNode = function (node) {
+		var children = [],
+			index,
+			jsonData,
+			childNode;
+
+		for (index = 0; index < node.childNodes.length; index++) {
+			childNode = node.childNodes[index];
+
+			if (childNode.localName !== null) {
+				children.push(parseNode(node.childNodes[index]));
+			}
+		}
+
+		var attributesString = "";
+		var attributesList = [];
+
+		if (node.attributes !== null && node.attributes.length > 0) {
+			for (index = 0; index < node.attributes.length; index++) {
+				attributesList.push(
+					node.attributes.item(index).localName + " = " +
+					node.attributes.item(index).nodeValue);
+			}
+			attributesString = ": " + attributesList.join(", ");
+		}
+
+		return {
+			"data" : (node.localName + attributesString),
+			"attr" : { "rel" : node.localName },
+			"children" : children
+		};
+	};
+
 	return {
 		init : function () {
+			// parse CSL file
+			var jsonData = [],
+				createNode = function (id) {
+					return {
+						"data" : "node " + id,
+						"attr" : {"rel" : "generic"}
+					};
+				},
+				index;
+
+			for (index = 0; index < 30; index++) {
+				jsonData.push(createNode(index));
+			}
+/*
+			$("#treeEditor").jstree({
+				"json_data" : { data : jsonData },
+				"types" : {
+					"valid_children" : [ "root" ],
+					"types" : {
+						"text" : {
+							"icon" : {
+								"image" : "http://static.jstree.com/v.1.0rc/_docs/_drive.png"
+							}
+						}
+					}
+				},
+					
+				"plugins" : ["themes","json_data","ui", "crrm", "dnd", "contextmenu", "types"],
+				// each plugin you have included can have its own config object
+				"core" : { "initially_open" : [ "node1" ] }
+				// it makes sense to configure a plugin only if overriding the defaults
+			});
+ */
 			CodeMirror.defaults.onChange = function()
 			{
 				clearTimeout(codeTimeout);
@@ -139,8 +284,11 @@ CSLEDIT.editorPage = (function () {
 			$.get(
 					styleURL, {}, function(data) { 
 					editor.setValue(data);
+					updateTreeView(data);
 				}
 			);
+
+			$("#editorTabs").tabs();
 		}
 	};
 }());
