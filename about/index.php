3b4733492a0d55698cd9e249e161ba00f4624c45
Steve Ridout <steveridout@gmail.com>
2012-03-30 17:07:16 +0200

    add google analytics code

--- about/index.php
+++ about/index.php
@@ -11,6 +11,18 @@
 	padding: 10px;
 }
 </style>
+<!-- google analytics -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-4601387-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </head>
 <body id="about">
 <?php include '../html/navigation.html'; ?>
--- visualEditor/index.php
+++ visualEditor/index.php
@@ -134,6 +134,18 @@ z-index: 30 !important;
 }
 
 </style>
+<!-- google analytics -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-4601387-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 </head>
 
 <body id="visualEditor">
