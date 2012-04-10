c70931f76f7280e751c38dc9fe92a318e5520f98
Steve Ridout <steveridout@gmail.com>
2012-04-10 18:02:12 +0200

    update README.md
    refs #62

--- about/index.php
+++ about/index.php
@@ -12,23 +12,33 @@
 }
 </style>
 <!-- google analytics -->
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-4601387-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
+<script type="text/javascript" src="../src/analytics.js"></script>
 </head>
 <body id="about">
 <?php include '../html/navigation.html'; ?>
 <div id="mainContent">
 <h1>Citation Style Editor</h1>
-The code for this site is on github: <a href="https://github.com/citation-style-editor">https://github.com/citation-style-editor</a>
+
+<p>This site allows you to search and edit citation styles. It's still a prototype.</p>
+
+<p>Source code: <a href="https://github.com/citation-style-editor">https://github.com/citation-style-editor</a></p>
+
+<p>Blog: <a href="http://csleditor.wordpress.com/">http://csleditor.wordpress.com/</a></p>
+
+<h2>Attributions</h2>
+
+<ul>
+<li><a href="http://citationstyles.org/">Citation Style Language</a></li>
+<li><a href="https://github.com/citation-style-language/styles">CSL style repository</a></li>
+<li><a href="http://gsl-nagoya-u.net/http/pub/citeproc-doc.html">citeproc-js</a> (Citation formatting engine)</li>
+<li><a href="http://www.jstree.com/">jsTree</a> (tree view on visualEditor page)</li>
+<li><a href="http://codemirror.net/">CodeMirror</a> (text editor on codeEditor page)</li>
+<li><a href="http://premiumsoftware.net/cleditor/">CLEditor</a> (rich text input on searchByExample page)</li>
+<li><a href="http://code.google.com/p/google-diff-match-patch/">diff_match_patch</a> (for showing highlighted differences in formatted output)</li>
+<li><a href="http://www.mozilla.org/rhino/">Rhino</a> js interpreter (for pre-calculating example citations on server)</li>
+<li><a href="http://www.thaiopensource.com/relaxng/trang.html">Trang</a> (for converting schema files from .rnc to .rng)</li>
+</ul>
+
 </div>
 </body>
 </html>
