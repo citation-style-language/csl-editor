6227e090e77cae89c6dadf5989ba3c8d6dbe1ba0
Steve Ridout <steveridout@gmail.com>
2012-05-16 15:54:31 +0200

    Add attributions
    refs #89

--- about/index.php
+++ about/index.php
@@ -37,14 +37,19 @@
 <li><a href="http://citationstyles.org/">Citation Style Language</a></li>
 <li><a href="https://github.com/citation-style-language/styles">CSL style repository</a></li>
 <li><a href="http://gsl-nagoya-u.net/http/pub/citeproc-doc.html">citeproc-js</a> (Citation formatting engine)</li>
-<li><a href="http://www.jstree.com/">jsTree</a> (tree view on visualEditor page)</li>
 <li><a href="http://codemirror.net/">CodeMirror</a> (text editor on codeEditor page)</li>
-<li><a href="http://premiumsoftware.net/cleditor/">CLEditor</a> (rich text input on searchByExample page)</li>
 <li><a href="http://code.google.com/p/google-diff-match-patch/">diff_match_patch</a> (for showing highlighted differences in formatted output)</li>
 <li><a href="http://www.mozilla.org/rhino/">Rhino</a> js interpreter (for pre-calculating example citations on server)</li>
 <li><a href="http://www.thaiopensource.com/relaxng/trang.html">Trang</a> (for converting schema files from .rnc to .rng)</li>
 <li><a href="http://www.famfamfam.com/lab/icons/silk/">FamFamFam Silk icons</a></li>
 <li><a href="http://p.yusukekamiyamane.com/">Fugue icons</a></li>
+<li><a href="http://jquery.com/">jQuery</a></li>
+<li><a href="http://www.jstree.com/">jQuery jsTree Plugin</a> (tree view on visualEditor page)</li>
+<li><a href="http://premiumsoftware.net/cleditor/">jQuery CLEditor Plugin</a> (rich text input on searchByExample page)</li>
+<li><a href="http://layout.jquery-dev.net">jQuery UI Layout Plugin</a></li>
+<li><a href="http://cherne.net/brian/resources/jquery.hoverIntent.html">jQuery hoverIntent Plugin</a></li>
+<li><a href="http://demos.flesler.com/jquery/scrollTo/">jQuery scrollTo Plugin</a></li>
+
 </ul>
 
 </div>
