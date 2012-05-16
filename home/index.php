86a420e6a83cde290515f6b692408db4956a4da6
Steve Ridout <steveridout@gmail.com>
2012-05-16 13:50:04 +0200

    Put homepage in /csl/home and use javascript redirect to get there from /csl/
    Not using /csl because people who have the old 301 redirect cached will never be able to open it.
    
    refs #89

---
+++ home/index.php
@@ -0,0 +1,37 @@
+<html>
+<head>	
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
+
+	<title>Citation Style Editor Homepage</title>
+
+	<link rel="stylesheet" href="css/base.css" />
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+<style>
+div#mainContainer {
+	margin: 20px;
+}
+</style>
+</head>
+<body id="homepage">
+<?php include '../html/navigation.html'; ?>
+
+<div id="mainContainer">
+	<h2>Know the name of the style you want?</h2>
+	<p>
+	Search for a style with <a href="/csl/searchByName">Search by Name</a>.
+	</p>
+
+	<h2>Know what the style should look like?</h2>
+	<p>
+	Search for a style by its output with <a href="/csl/searchByExample">Search by Example</a>. If you don't find an <strong>exact</strong> match you will have the option to edit a similar style.
+	</p>
+
+	<h2>Want to create a new style from scratch?</h2>
+	<p>This is <strong>not recommended</strong>. It's much easier to <a href="/csl/searchByExample">Search by Example</a> to find a similar style first, which you can then edit.
+	</p>
+	<p>
+	If you really want to start a new style, you can create a new one with the <a href="/csl/visualEditor/?styleURL=http://<?php echo $_SERVER['HTTP_HOST']; ?>/csl/content/newStyle.csl" >Visual Editor</a>.
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,37 +1,14 @@
 <html>
-<head>	
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> 
-
-	<title>Citation Style Editor Homepage</title>
-
-	<link rel="stylesheet" href="css/base.css" />
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-
-<style>
-div#mainContainer {
-	margin: 20px;
-}
-</style>
+<head>
+<script type="text/javascript">
+"use strict";
+// javascript redirect
+// (this is not permentantly cached like 301 redirects)
+window.location.pathname = "/csl/home";
+</script>
 </head>
-<body id="homepage">
-<?php include 'html/navigation.html'; ?>
-
-<div id="mainContainer">
-	<h2>Know the name of the style you want?</h2>
-	<p>
-	Search for a style with <a href="/csl/searchByName">Search by Name</a>.
-	</p>
-
-	<h2>Know what the style should look like?</h2>
-	<p>
-	Search for a style by its output with <a href="/csl/searchByExample">Search by Example</a>. If you don't find an <strong>exact</strong> match you will have the option to edit a similar style.
-	</p>
-
-	<h2>Want to create a new style from scratch?</h2>
-	<p>This is <strong>not recommended</strong>. It's much easier to <a href="/csl/searchByExample">Search by Example</a> to find a similar style first, which you can then edit.
-	</p>
-	<p>
-	If you really want to start a new style, you can create a new one with the <a href="/csl/visualEditor/?styleURL=http://<?php echo $_SERVER['HTTP_HOST']; ?>/csl/content/newStyle.csl" >Visual Editor</a>.
-</div>
+<body>
+<p>If you are not automatically redirected, this site probably won't work for you.</p>
+<p>Please use the latest version of Chrome or Firefox and ensure javascript is enabled.</p>
 </body>
 </html>
