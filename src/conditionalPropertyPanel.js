16a2e6858e1a9e6a06ba60118181ea4c9483b93c
Steve Ridout <steveridout@gmail.com>
2012-06-21 18:34:55 +0200

    Don't allow the same value twice in the conditional property panel.
    Default subOption for "The variable" is now "is present"
    
    refs #117

--- src/conditionalPropertyPanel.js
+++ src/conditionalPropertyPanel.js
@@ -43,9 +43,65 @@ CSLEDIT.conditionalPropertyPanel.prototype.attributeValue = function () {
 	return values.join(" ");
 };
 
+CSLEDIT.conditionalPropertyPanel.prototype.removeDuplicateOptions = function () {
+	var that = this,
+		selectedValues = this.attributeValue().split(" "),
+		processedValues = [],
+		availableValues;
+
+	// remove currently selcted values from availableValues
+	availableValues = this.possibleValues(this.currentAttribute);
+	$.each(selectedValues, function (i, value) {
+		var index = availableValues.indexOf(value);
+		if (index !== -1) {
+			availableValues.splice(index, 1);
+		}
+	});
+
+	// remove duplicate values
+	$.each(this.valueControls, function (i, valueControl) {
+		var $this = $(this),
+			value = $this.val();
+
+		// check if it's a duplicate
+		if (processedValues.indexOf(value) !== -1) {
+			if (availableValues.length === 0) {
+				// no more available values, set processedValues and setup again
+				that.node.setAttr(that.currentAttribute, processedValues.join(" "));
+				CSLEDIT.controller.exec('amendNode', [that.node.cslId, that.node]);
+				that.setup();
+				return;
+			} else {
+				// give it a new value
+				value = availableValues.pop();
+				$this.val(value);
+				selectedValues.push(value);
+			}
+		}
+		processedValues.push(value);
+	});
+
+	// remove currently selected values from other controls options
+	$.each(this.valueControls, function (i, valueControl) {
+		var $this = $(this);
+
+		// remove all except current val
+		$this.find('option[value!="' + $this.val() + '"]').remove();
+
+		// add back the other available options
+		$.each(availableValues, function (i, option) {
+			$this.append('<option>' + option + '</option>');
+		});
+	});
+};
+
 CSLEDIT.conditionalPropertyPanel.prototype.attributeUi = {
 	"disambiguate" : {
 		mainOption: "Citations are disambiguated"
+	},
+	"variable" : {
+		mainOption: "The variable",
+		subOption: "is present"
 	},
 	"is-numeric" : {
 		mainOption: "The variable",
@@ -64,10 +120,6 @@ CSLEDIT.conditionalPropertyPanel.prototype.attributeUi = {
 	},
 	"type" : {
 		mainOption: "The document type is"
-	},
-	"variable" : {
-		mainOption: "The variable",
-		subOption: "is present"
 	}
 };
 
@@ -149,6 +201,8 @@ CSLEDIT.conditionalPropertyPanel.prototype.setup = function () {
 
 	this.drawControls();
 
+	this.removeDuplicateOptions();
+
 	// event handlers
 	this.mainOptionSelect.on('change', function () {
 		console.log("change");
@@ -184,7 +238,7 @@ CSLEDIT.conditionalPropertyPanel.prototype.setup = function () {
 	}
 
 	this.element.find('select.valueSelect').on('change', function () {
-		console.log('setting ' + that.currentAttribute + ' to ' + that.attributeValue());
+		that.removeDuplicateOptions();
 		that.node.setAttr(that.currentAttribute, that.attributeValue());
 		CSLEDIT.controller.exec('amendNode', [that.node.cslId, that.node]);
 	});
