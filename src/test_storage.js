d689cf58d2c208a91696db240c5d2f7db0624b83
Steve Ridout <steveridout@gmail.com>
2012-06-29 17:34:28 +0200

    add test for callback when data is out of sync
    refs #130

--- src/test_storage.js
+++ src/test_storage.js
@@ -26,3 +26,23 @@ test("clear/set/get/remove items", function () {
 	doTests(new CSLEDIT.Storage(false), "with simple storage");
 	doTests(new CSLEDIT.Storage(true), "with local storage");
 });
+
+test("sync checking", function () {
+	var message = "",
+		storage = new CSLEDIT.Storage(true, function () {
+			message = "out of sync";
+		});
+
+	storage.clear();
+	storage.setItem("item1", "set by this tab");
+
+	// use localStorage directly to simulate "item1" being set by a different tab
+	localStorage.setItem("item1", "set by different tab");
+
+	equal(message, "");
+
+	// now this should trigger the callback
+	storage.getItem("item1");
+
+	equal(message, "out of sync");
+});
