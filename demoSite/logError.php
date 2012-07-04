89e3e137d296153e1a7a411a57df77cdaa038400
SteveRidout <steveridout@gmail.com>
2012-07-04 17:31:25 +0200

    use 24hr format time in error logs
    refs #133

--- demoSite/logError.php
+++ demoSite/logError.php
@@ -1,6 +1,6 @@
 <?php
 // write to error.log file
-$success = error_log("\nDate: " . date('d.m.Y h:i:s') . "\n" . $_REQUEST['message'] . "\n", 3, "error.log");
+$success = error_log("\nDate: " . date('d.m.Y H:i:s') . "\n" . $_REQUEST['message'] . "\n", 3, "error.log");
 if ($success == TRUE) {
         echo "success";
 } else {
