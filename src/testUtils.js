44c37870f57e4b1217fdc5dfca204ab04e0edd02
SteveRidout <steveridout@gmail.com>
2012-07-06 09:49:45 +0200

    forgot testUtils

---
+++ src/testUtils.js
@@ -0,0 +1,52 @@
+"use strict";
+var CSLEDIT = CSLEDIT || {};
+
+CSLEDIT.testUtils = {};
+
+// returns a list of complete CSL styles from the repository
+//
+// returns only top styles at present
+CSLEDIT.testUtils.getStyles = function (maxNumberOfStyles) {
+	var styles = {};
+
+	$.each(CSLEDIT.exampleData.topStyles, function (i, style) {
+		var url = CSLEDIT.options.get("rootURL") + '/external/csl-styles/' +
+				style.replace(/^.*\//, "") + ".csl",
+			cslCode;
+
+		if (i >= maxNumberOfStyles) {
+			// we've got enough
+			return false;
+		}
+
+		console.log("style url = " + url);
+
+		$.ajax({
+			url : url,
+			success : function (data) {
+				cslCode = data;
+			},
+			async : false
+		});
+
+		if (typeof(cslCode) === "undefined") {
+			// try dependent style
+			 url = CSLEDIT.options.get("rootURL") + '/external/csl-styles/dependent/' +
+				style.replace(/^.*\//, "") + ".csl";
+	
+			$.ajax({
+				url : url,
+				success : function (data) {
+					cslCode = data;
+				},
+				async : false
+			});
+		}
+
+		ok(typeof(cslCode) !== "undefined", "loaded " + url);
+		styles[url] = cslCode;
+	});
+
+	return styles;
+};
+
