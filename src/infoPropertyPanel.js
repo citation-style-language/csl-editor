af392f30484bab30a10f0950ce64a9c4c41aa6ed
SteveRidout <steveridout@gmail.com>
2012-07-12 18:33:18 +0200

    make textValue input boxes bigger in info property panel

--- src/infoPropertyPanel.js
+++ src/infoPropertyPanel.js
@@ -170,12 +170,15 @@ CSLEDIT.infoPropertyPanel = (function () {
 	};
 
 	var editorRow = function (item, node, schemaAttributes) {
+		var row;
 		if (attributeNodes.indexOf(item.node) >= 0) {
 			return attributeEditorRow(item, node, schemaAttributes);
 		} else if (nameNodes.indexOf(item.node) >=0) {
 			return nameEditorRow(item, node);
 		} else {
-			return textValueEditorRow(item, node);
+			row = textValueEditorRow(item, node);
+			row.find("input").css({width:"400px"});
+			return row;
 		}
 	};
 
