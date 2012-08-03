5ed7b363b10be0a485d7f8295bce90241eb8c06b
Steve Ridout <steveridout@gmail.com>
2012-08-03 15:58:22 +0200

    update configure.sh to run node instead of rhino

--- configure.sh
+++ configure.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# write current git commit
-mkdir generated
-git rev-parse HEAD > generated/commit.txt
-
 # generate example citations
 cd server
-java -jar "../external/rhino/js-1.7R2.jar" -opt -1 generateExampleCitations.js
+node generateExampleCitations.js
 cd ..
 
 # convert schema from .rnc (Relax NG Compact) to .rng (Relax NG XML)
