9f68c10c60767793639c305db49841421104f012
Steve Ridout <steveridout@gmail.com>
2012-08-14 12:20:57 +0200

    add missing dataInstance file

---
+++ src/dataInstance.js
@@ -0,0 +1,15 @@
+"use strict";
+
+define(['src/Data'], function (CSLEDIT_Data) {
+	// global instance
+	return CSLEDIT_Data("CSLEDIT.cslData", [
+		"style",
+		"style/info",
+		"style/info/title",
+		"style/info/id",
+		"style/citation",
+		"style/citation/layout",
+		"style/bibliography",
+		"style/bibliography/layout"
+	], true);
+});
