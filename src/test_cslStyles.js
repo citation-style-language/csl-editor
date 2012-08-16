64d322e4cdc600df2b3056487318aaf5404acda6
Steve Ridout <steveridout@gmail.com>
2012-08-16 12:34:32 +0200

    forgot to add test

---
+++ src/test_cslStyles.js
@@ -0,0 +1,63 @@
+define(
+		[	'src/VisualEditor',
+			'src/cslStyles'
+		],
+		function (
+			CSLEDIT_VisualEditor,
+			CSLEDIT_cslStyles
+		) {
+	module('CSLEDIT_VisualEditor');
+
+	test('styles', function () {
+		var defaultMasterId = CSLEDIT_cslStyles.styles().masterIdFromId[CSLEDIT_cslStyles.defaultStyleId],
+			numStyles = Object.keys(CSLEDIT_cslStyles.styles().masterIdFromId).length;
+		
+		equal(defaultMasterId, CSLEDIT_cslStyles.defaultStyleId, "default style is master");
+
+		ok(numStyles > 2000, "enough styles");
+		equal(numStyles, Object.keys(CSLEDIT_cslStyles.styles().styleTitleFromId).length, "enough titles");
+		
+		console.log("Total number of styles: " + numStyles);
+	});
+
+	test('examples', function () {
+		ok(Object.keys(CSLEDIT_cslStyles.exampleCitations().exampleCitationsFromMasterId).length > 500);
+
+		console.log("Number of example citations: " +
+			CSLEDIT_cslStyles.exampleCitations().exampleCitationsFromMasterId.length);
+	});
+
+	test('style id generation', function () {
+		var numSuccesses = 0,
+			failures = [],
+			successRate,
+			tolerance = 1; //0.86;
+
+		// check that we generate titles the same as the existing repo ones
+		$.each(CSLEDIT_cslStyles.styles().styleTitleFromId, function (id, title) {
+			var generatedId = CSLEDIT_cslStyles.generateStyleId(title);
+			
+			if (generatedId === id) {
+				numSuccesses++;
+			} else {
+				failures.push({
+					title : title,
+					id : id,
+					generatedId : generatedId
+				});
+			}
+		});
+
+		successRate = numSuccesses / (numSuccesses + failures.length);
+		console.log("success rate = " + successRate);
+		ok(successRate > tolerance, "success rate = " + Math.round(successRate * 100) +
+				"% (minimum tolerance = " + Math.round(100 * tolerance) + "%)");
+
+		if (successRate <= tolerance) {
+			// show failures
+			$.each(failures, function (i, fail) {
+				equal(fail.generatedId, fail.id, fail.title);
+			});
+		}
+	});
+});
