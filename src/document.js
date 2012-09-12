07c606defb7e513eea9261c976bdfb88eb898717
Steve Ridout <steveridout@gmail.com>
2012-09-12 20:51:55 +0200

    Add 'used by' dependents
    Remove 'src/' for less clutter

--- src/document.js
+++ src/document.js
@@ -98,6 +98,10 @@ define(['src/urlUtils'], function (CSLEDIT_urlUtils) {
 		populatePageData(resourceQueue, sourceFiles, processedResources);
 	};
 
+	var cleanName = function (name) {
+		return name.replace('src/', '');
+	};
+
 	var generate = function (resources, element) {
 		element = $(element);
 
@@ -106,9 +110,19 @@ define(['src/urlUtils'], function (CSLEDIT_urlUtils) {
 		var sourceFiles = [];
 		populatePageData(resources, sourceFiles);
 
+		sourceFiles.sort(function (a, b) {
+			if (a.name < b.name) {
+				return -1;
+			} else if (a.name > b.name) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+
 		// draw page
 		$.each(sourceFiles, function (i, sourceFile) {
-			element.append('<h3><a name="' + sourceFile.name + '" href="#' + sourceFile.name + '">' + sourceFile.name + '</a></h3>');
+			element.append('<h3><a name="' + sourceFile.name + '" href="#' + sourceFile.name + '">' + cleanName(sourceFile.name) + '</a></h3>');
 			var moduleInfo = $('<div/>').addClass("moduleInfo");
 
 			if (sourceFile.description.length === 0 ||
@@ -117,13 +131,27 @@ define(['src/urlUtils'], function (CSLEDIT_urlUtils) {
 			} else {
 				moduleInfo.append('<p>' + sourceFile.description.join("</p><p>") + '</p>');
 			}
-			moduleInfo.append('<strong>dependencies: </strong>');
+
 			var depList = [];
 			$.each(sourceFile.dependencies, function (i, dep) {
-				depList.push('<a href="#' + dep + '">' + dep + '</a>');
+				depList.push('<a href="#' + dep + '">' + cleanName(dep) + '</a>');
 			});
-			moduleInfo.append(depList.join(", "));
+			if (depList.length > 0) {
+				moduleInfo.append('<p><strong>dependencies: </strong>' + depList.join(", ") + '</p>');
+			}
+
+			var usedBy = [];
+			$.each(sourceFiles, function (i, sourceFile2) {
+				if (sourceFile2.dependencies.indexOf(sourceFile.name) !== -1) {
+					usedBy.push('<a href="#' + sourceFile2.name + '">' + cleanName(sourceFile2.name) + '</a>');
+				}
+			});
+			if (usedBy.length > 0) {
+				moduleInfo.append('<p><strong>used by: </strong>' + usedBy.join(", ") + '</p>');
+			}
+
 			moduleInfo.append('<p><a href="' + sourceFile.url + '">View Code</a></p>');
+
 			element.append(moduleInfo);
 		});
 	};
